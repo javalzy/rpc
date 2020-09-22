33c7fc5e08a5b7c50a849e88b7b00b1d220cb582
lzymac <javalzy@126.com>
2020-09-22 13:53:14 +0800

    Merge remote-tracking branch 'upstream/master'

--- mergeFromRemote.sh
+++ mergeFromRemote.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git remote -v
-git remote add upstream https://github.com/tars-node/rpc.git
+git remote add --track master upstream https://github.com/tars-node/rpc.git
 git fetch upstream
 git merge upstream/master
 git push
